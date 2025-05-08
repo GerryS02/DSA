2105b2712821e79c409a93c04ab3fa6241e01c32
Gerry Summers <Singur95878@student.edmonds.edu>
2025-05-08 14:23:00 -0700

    added a new solution

---
+++ TwoPointers/TwoDifference.java
@@ -0,0 +1,35 @@
+package DSA.TwoPointers;
+
+import java.util.Arrays;
+
+public class TwoDifference {
+    public static class Solution{
+        public boolean findPair(int[] arr, int x){
+            int n = arr.length;
+
+            Arrays.sort(arr);
+
+            int j = 1;
+
+            for(int i=0; i<n; i++){
+                while(j < n && arr[j] - arr[i] < x){
+                    j++;
+                }
+
+                while(j<n && i != j && arr[j] - arr[i] == x){
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static void main(String[] args){
+            int[] arr = {5, 20, 3, 2, 50, 80};
+            int x = 78;
+
+            Solution sol = new Solution();
+
+            System.out.println(sol.findPair(arr, x));
+        }
+    }
+}
