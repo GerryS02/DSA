2ffb2c56b1e75a44418e4d4498922c0ea7c640fb
Gerry Summers <Singur95878@student.edmonds.edu>
2025-07-08 14:19:21 -0700

    fixed errors

---
+++ HashMap/Longest Consecutive Sequence/LongestSequence.cpp
@@ -0,0 +1,7 @@
+class Solution{
+public: 
+    int longestSequence(int[] arr){
+        unordered_set<int> set;
+        
+    }
+};
--- HashMap/Subarray sum equals k/SubarraySum.java
+++ HashMap/Subarray sum equals k/SubarraySum.java
@@ -1,6 +1,6 @@
 import java.util.HashMap;
 
-class Solution{
+class SubarraySum{
     public int subarraySum(int[] arr, int k){
         int n = arr.length;
         int count = 0;
