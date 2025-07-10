df5a0266938278659f053e7ddf2395db675c7d48
Gerry Summers <Singur95878@student.edmonds.edu>
2025-07-09 20:14:04 -0700

    added a new solution

---
+++ Heap/Kth Largest Element in an Array/findKthLargest.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+class Solution{
+public:
+    int finfKthLargest(int[] arr, int k){
+        //make a  (min-heap) priority queue where smallest element is at the top
+        priority_queue<int, vector<int>, greater<int>> pq;
+
+        for(int i=0; i<arr.size(); i++){
+            if(i<k){
+                pq.push(arr[i]); //add the first k elements to the heap
+            } else{
+                //if the current element is greater than the smallest in the heap
+                if(arr[i] > pq.top()){
+                    pq.pop(); //remove the smallest element
+                    pq.push(arr[i]); //add the current element
+                }
+            }
+        }
+
+        return pq.top(); //return the root (kth largest element)
+    }
+
+    int main(){
+        Solution sol;
+
+        int[] arr = {3,2,1,5,6,4};
+        int k = 3;
+
+        int ans = sol.finfKthLargest(arr, k);
+
+        cout<<ans<<endl;
+
+        return 0;
+    }
+};
