6b39c4a976f32b28366ba93edc6da8e9e4ed492e
Gerry Summers <Singur95878@student.edmonds.edu>
2025-07-10 22:42:15 -0700

    added a new problem

--- Heap/Minimum Cost to Connect Sticks/ConnectSticks.java
+++ Heap/Minimum Cost to Connect Sticks/ConnectSticks.java
@@ -1,5 +1,42 @@
-
+import java.util.PriorityQueue;
 
 public class ConnectSticks {
-    
+    public int connectSticks(int[] arr){
+        //priority queue to get the smallest sticks first
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        //add all the sticks to the queue
+        for(int stick : arr){
+            pq.add(stick);
+        }
+
+        int totCost = 0; //total cost of connecting the sticks
+
+        //until one stick left, combine two smallest sticks
+        while(pq.size() > 1){
+            //take out two smallest sticks
+            int s1 = pq.remove();
+            int s2 = pq.remove();
+
+            //cost to connect two sticks
+            int cost = s1 + s2;
+
+            //add to the total cost
+            totCost += cost;
+
+            //add the new stick back to the queue
+            pq.add(cost);
+        }
+
+        return totCost;
+    }
+
+    public static void main(String[] args){
+        ConnectSticks cs = new ConnectSticks();
+
+        int[] sticks = {1,5,9,11,12,50,30,2,6};
+        int totalCost = cs.connectSticks(sticks);
+
+        System.out.println(totalCost);
+    }
 }
